719bd00b4b70cfc198ae8d4a54bc9e2f35eb7378
josmartinezbe <sebastianmartinez66@gmail.com>
2025-07-18 18:51:07 -0500

    se creo el programa

---
+++ Miprimerprograma.java
@@ -0,0 +1,12 @@
+public class Miprimerprograma{
+	public static void sumar(int a, int b){
+			
+	}
+	public static int suma(int a, int b){
+		return a+b;	
+	}
+	public static void main(String []args){
+		sumar(5,4);
+		System.out.println(suma(6,4));		
+	}
+}
